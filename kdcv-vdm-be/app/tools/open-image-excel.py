d49ce1e8cfa4db367f8d151136d2878066177a8d
tannhat22 <nguyentannhat2298@gmail.com>
2024-06-26 18:08:26 +0700

    test 123

--- kdcv-vdm-be/app/tools/open-image-excel.py
+++ kdcv-vdm-be/app/tools/open-image-excel.py
@@ -1,53 +1,5 @@
-# from openpyxl import load_workbook
-# from openpyxl_image_loader import SheetImageLoader
-# import os
-
-
-# file_name = "T24Y003V00-VDM001-AB.xlsx"
-# folder_path = "/home/amrserver/Documents/KDCV-m104/xlsx"
-
-# def find_all_xlsx_in_folder(folder_path):
-#     listFile = []
-#     for filename in os.listdir(folder_path):
-#         # Kiểm tra xem file có phần mở rộng .xlsb hay không
-#         if filename.endswith(('.xlsx','.XLSX')):
-#             # Tạo đường dẫn đầy đủ tới file .xlsb
-#             xlsb_file = os.path.join(folder_path, filename)
-#             listFile.append(xlsb_file)
-#     return listFile
-
-# # Load your workbook and sheet as you want, for example
-# wb = load_workbook(os.path.join(folder_path, file_name))
-# sheet = wb['KTV (2)']
-
-# allFileXLSX = find_all_xlsx_in_folder(folder_path)
-# # for file in allFileXLSX:
-# #     wb = load_workbook(file)
-# #     wb.close()
-
-
-# # Put your sheet in the loader
-# image_loader = SheetImageLoader(sheet)
-
-
-# # # # And get image from specified cell
-# image = image_loader.get('E5')
-
-# print(image.info)
-# # # # Image now is a Pillow image, so you can do the following
-# image.show()
-
-# # # Ask if there's an image in a cell
-# # if image_loader.image_in('E5'):
-# #     print("Got it!")
-# # else:
-# #     print("image not in cell you choose!")
-
-
-
-
 from openpyxl import load_workbook
-from openpyxl.drawing.image import Image
+from openpyxl_image_loader import SheetImageLoader
 
 # Đường dẫn đến file Excel của bạn
 file_path = '/home/amrserver/Documents/KDCV-m104/xlsx/T24Y003V00-VDM001-AB.xlsx'
@@ -58,24 +10,33 @@ wb = load_workbook(filename=file_path)
 # Chọn sheet bạn muốn đọc
 ws = wb['KTV (2)']  # Thay 'Sheet1' bằng tên sheet của bạn
 
-# Danh sách các hình ảnh
-images = []
+# Tạo một SheetImageLoader
+image_loader = SheetImageLoader(ws)
 
-# Duyệt qua tất cả các hình ảnh trong sheet
-for image in ws._images:
-    # Lấy tọa độ của hình ảnh
-    anchor = image.anchor._from
-    # Chuyển tọa độ thành địa chỉ ô
-    cell_address = ws.cell(anchor.row + 1, anchor.col + 1).coordinate
-    images.append((cell_address, image))
+# Lấy danh sách các vùng ô hợp nhất
+merged_cells = ws.merged_cells.ranges
 
-# Duyệt qua các hình ảnh và in thông tin
-for idx, (cell_address, image) in enumerate(images):
-    print(f'Hình ảnh tại ô {cell_address}')
-    # Nếu bạn muốn lưu hình ảnh
-    image_path = f'image_from_{cell_address}_{idx}.png'
-    with open(image_path, 'wb') as img_file:
-        img_file.write(image._data())
+# Hàm để kiểm tra xem một ô có nằm trong vùng ô hợp nhất hay không
+def get_merged_cell_range(cell):
+    for merged_range in merged_cells:
+        if cell.coordinate in merged_range:
+            return merged_range
+    return None
+
+# Danh sách các ô có chứa hình ảnh
+image_cells = list(image_loader._images.keys())
+
+# Duyệt qua các ô có chứa hình ảnh và lưu chúng
+for cell in image_cells:
+    merged_range = get_merged_cell_range(ws[cell])
+    if merged_range:
+        print(merged_range.coord)
+        cell_address = merged_range.coord.split(':')[0]
+    else:
+        cell_address = cell
+    image = image_loader.get(cell)
+    image_path = f'image_from_{cell_address}_{image_cells.index(cell)}.png'
+    # image.save(image_path)
     print(f'Hình ảnh từ ô {cell_address} đã được lưu tại {image_path}')
 
 # Đóng workbook
